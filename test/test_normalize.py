491ae2b023c11bc81713d6c0743f3c1c15514007
Rob Speer <rspeer@mit.edu>
2010-06-14 13:34:10 -0400

    remove old test code

--- test/test_normalize.py
+++
@@ -1,4 +0,0 @@
-from csc.conceptnet4.models import *
-def test_normalize():
-    assert en.nl.normalize('they are running') == 'run'
-    assert en.nl.normalize('went') == 'go'
--- test/test_users.py
+++
@@ -1,6 +0,0 @@
-from csc.conceptnet4.models import *
-
-def test_users_do_not_explode():
-    a = RawAssertion.objects.filter(language=en)[0]
-    a.sentence.creator
-    a.sentence.creator.username
