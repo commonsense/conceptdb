25a4758221ac546e4db3e14b6531f0d9e2caa9c0
Rob Speer <rspeer@mit.edu>
2010-09-22 17:28:16 -0400

    Reasons now have their own table, suitable for MapReduce.

--- conceptdb/build/conceptnet.py
+++ conceptdb/build/conceptnet.py
@@ -3,7 +3,8 @@ from csc.conceptnet.models import Assertion as OldAssertion, RawAssertion, Vote,
 
 import conceptdb
 from conceptdb.assertion import Assertion, Expression
-from conceptdb.metadata import Dataset, ExternalReason
+from conceptdb.justify import Reason, justify
+from conceptdb.metadata import Dataset
 
 import time
 import logging
@@ -19,26 +20,26 @@ DATASET_ROOT = '/data/conceptnet/4/'
 def import_contributors(lang):
     dataset = Dataset.make(DATASET_ROOT+lang, lang)
     root = dataset.get_root_reason()
-    site = root.derived_reason('/site/omcs', weight=1.0)
+    site = dataset.get_reason('/site/omcs')
+    justify(site, [(root, 1.0)])
     for user in User.objects.iterator():
-        voter = site.derived_reason(CONTRIBUTOR_ROOT+user.username, weight=0.5)
-        voter.save()
-        log.info(voter)
+        contributor = dataset.get_reason(CONTRIBUTOR_ROOT+user.username)
+        justify(contributor, [(site, 0.5)])
+        log.info(contributor)
 
 def import_activities(lang):
     dataset = Dataset.make(DATASET_ROOT+lang, lang)
-    root = dataset.get_root_reason()
-    site = root.derived_reason('/site/omcs', weight=1.0)
+    site = dataset.get_reason('/site/omcs')
     for act in Activity.objects.iterator():
-        newact = site.derived_reason(ACTIVITY_ROOT+act.name.replace(' ', '_'), weight=1.0)
-        newact.save()
+        newact = dataset.get_reason(ACTIVITY_ROOT+act.name.replace(' ', '_'))
+        justify(newact, [(site, 1.0)])
         log.info(newact)
 
 def import_assertions(lang):
     dataset = Dataset.make(DATASET_ROOT+lang, lang)
-    root = dataset.get_root_reason()
-    site = root.derived_reason('/site/omcs', weight=1.0)
-    generalize_reason = root.derived_reason('/rule/generalize', weight=0.1)
+    generalize_reason = dataset.get_reason('/rule/generalize')
+    justify(generalize_reason, [(dataset.get_root_reason(), 0.1)])
+
     assertions = OldAssertion.objects.filter(score__gt=0, language__id=lang)\
       .select_related('concept1', 'concept2', 'relation', 'language')[:100]
     for assertion in assertions:
@@ -53,18 +54,18 @@ def import_assertions(lang):
         newassertion = Assertion.make(dataset, relation, concepts,
                                          polarity = assertion.polarity,
                                          context=context)
-        sent_contributors = set()
+        newassertion.save()
         
+        sent_contributors = set()
         support_votes = assertion.votes.filter(vote=1)
         oppose_votes = assertion.votes.filter(vote=-1)
         for vote in support_votes:
-            voter = dataset.get_reason_name(CONTRIBUTOR_ROOT+vote.user.username)
+            voter = dataset.get_reason(CONTRIBUTOR_ROOT+vote.user.username)
             if voter not in sent_contributors:
                 newassertion.add_support([voter])
         for vote in oppose_votes:
-            voter = dataset.get_reason_name(CONTRIBUTOR_ROOT+vote.user.username)
+            voter = dataset.get_reason(CONTRIBUTOR_ROOT+vote.user.username)
             newassertion.add_oppose([voter])
-        newassertion.save()
 
         for raw in raws:
             if raw.score > 0:
@@ -73,18 +74,18 @@ def import_assertions(lang):
                 support_votes = raw.votes.filter(vote=1).select_related('user')
                 oppose_votes = raw.votes.filter(vote=-1).select_related('user')
                 for vote in support_votes:
-                    voter = dataset.get_reason_name(CONTRIBUTOR_ROOT+vote.user.username)
+                    voter = dataset.get_reason(CONTRIBUTOR_ROOT+vote.user.username)
                     expr.add_support([voter])
                 for vote in oppose_votes:
-                    voter = dataset.get_reason_name(CONTRIBUTOR_ROOT+vote.user.username)
+                    voter = dataset.get_reason(CONTRIBUTOR_ROOT+vote.user.username)
                     expr.add_oppose([voter])
                 expr.save()
 
                 sent = raw.sentence
                 if sent.score > 0:
                     activity = sent.activity.name
-                    act_reason = dataset.get_reason_name(ACTIVITY_ROOT+activity.replace(' ', '_'))
-                    voter = dataset.get_reason_name(CONTRIBUTOR_ROOT+vote.user.username)
+                    act_reason = dataset.get_reason(ACTIVITY_ROOT+activity.replace(' ', '_'))
+                    voter = dataset.get_reason(CONTRIBUTOR_ROOT+vote.user.username)
 
                     sent_contributors.add(voter)
                     justification = [act_reason, voter]
--- conceptdb/metadata.py
+++ conceptdb/metadata.py
@@ -1,6 +1,6 @@
 from csc.nl import get_nl
 from conceptdb import ConceptDBDocument
-from conceptdb.justify import Justification, ConceptDBJustified
+from conceptdb.justify import ConceptDBJustified
 from mongoengine.queryset import DoesNotExist
 import mongoengine as mon
 
@@ -25,108 +25,8 @@ class Dataset(ConceptDBDocument, mon.Document):
         assert self.name.startswith('/data/')
 
     def get_reason(self, reason_name):
-        if isinstance(reason_name, ConceptDBDocument):
-            assert reason_name.dataset == self.name
-            return reason_name
-        else:
-            assert isinstance(reason_name, basestring)
-            if reason_name.startswith('/assertion/'):
-                from conceptdb.assertion import Assertion
-                parts = reason_name.split('/')
-                a_name = parts[2]
-                assertion = Assertion.objects.with_name(a_name)
-                assert assertion.dataset == self.name
-                return assertion
-            elif reason_name.startswith('/data/'):
-                assert reason_name.startswith(self.name)
-                return ExternalReason.objects.with_name(reason_name)
-            else:
-                full_name = self.name+reason_name
-                return ExternalReason.objects.with_name(full_name)
-    
-    def get_reason_name(self, reason_name):
         return self.name+reason_name
 
     def get_root_reason(self):
-        return ExternalReason.make(self.name, '/root')
-    
-EXT_REASON_TYPES = ['root', 'admin', 'site', 'contributor', 'rule', 'activity']
-class ExternalReason(mon.Document, ConceptDBJustified):
-    """
-    An ExternalReason is a unit of justification. It indicates a reason to
-    believe some ConceptDBJustified object, when that reason is represented
-    outside of ConceptDB.
+        return self.get_reason('/root')
 
-    ExternalReasons must be associated with a particular dataset, so that they
-    are consistent with Assertions, and so that they can be synced between
-    different ConceptDBs independently.
-
-    Assertions are not the same as Reasons, but they may also be used as
-    units of justification. Use justify.lookup_reason() to get the appropriate
-    Assertion or Reason from an ID.
-    """
-    name = mon.StringField(required=True, primary_key=True)
-    justification = mon.EmbeddedDocumentField(Justification)
-    dataset = mon.StringField(required=True)
-    
-    meta = {'indexes': ['dataset', 'name',
-                        'justification.support_flat',
-                        'justification.oppose_flat',
-                        'justification.confidence_score',
-                       ]}
-    
-    @staticmethod
-    def make(dataset, name_suffix, reasons=None):
-        needs_save = False
-        if isinstance(dataset, basestring):
-            datasetObj = Dataset.get(dataset)
-        else:
-            datasetObj = dataset
-            dataset = datasetObj.name
-        name = dataset + name_suffix
-        try:
-            r = ExternalReason.get(name)
-        except DoesNotExist:
-            r = ExternalReason(
-                name=name,
-                dataset=dataset,
-                justification=Justification.empty(),
-               )
-            if name_suffix == '/root':
-                r.justification.confidence_score = 1.0
-            needs_save = True
-        if reasons is not None:
-            r.add_support(reasons)
-            needs_save = True
-        if needs_save:
-            r.update_confidence()
-            r.save()
-        return r
-
-    def update_confidence(self):
-        if self.name_suffix() == '/root': return
-        ConceptDBJustified.update_confidence(self)
-
-    def check_consistency(self):
-        assert self.name.startswith(self.dataset)
-        assert self.name_suffix().startswith('/')
-        assert self.type() in EXT_REASON_TYPES
-        self.justification.check_consistency()
-        self.get_dataset()
-    
-    def get_dataset(self):
-        return Dataset.objects.with_id(self.dataset)
-
-    def name_suffix(self):
-        return self.name[len(self.dataset):]
-
-    def derived_reason(self, name_suffix, weight=1.0):
-        return ExternalReason.make(self.dataset, name_suffix, [(self, weight)])
-
-    def type(self):
-        return self.name_suffix().split('/')[1]
-    
-    def __str__(self):
-        return "<ExternalReason: %s>" % self.name
-    def __repr__(self):
-        return "<ExternalReason: %s>" % self.name
