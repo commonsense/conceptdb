9471cfe08fa4055306062d3d8c661f058d77c077
Elizabeth L. Hawkins <elhawk@mit.edu>
2010-10-04 17:35:24 -0400

    test_dataset.py now working with new code

--- conceptdb/test/test_dataset.py
+++ conceptdb/test/test_dataset.py
@@ -1,9 +1,13 @@
 import conceptdb
-from conceptdb.metadata import Dataset, ExternalReason
+from conceptdb.metadata import Dataset 
 
 conceptdb.connect_to_mongodb('test')
 
 def test_dataset():
+
+    #start with an empty collection
+    Dataset.drop_collection()
+
     dataset = Dataset.make(language='en', name='/data/test')
     
     # Make sure it was saved to the database
@@ -16,9 +20,7 @@ def test_dataset():
     #test methods
     reason = dataset.get_root_reason()
 
-    assert reason.name == '/data/test/root'
-    
-    assert dataset.get_reason(reason) == reason
+    assert '/data/test/root' == reason
 
     dataset.nl
 
