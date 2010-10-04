4c0b64df97225cab3e8fac14cda702f283eca8c9
Elizabeth L. Hawkins <elhawk@mit.edu>
2010-10-04 17:52:29 -0400

    test_expression works with new expressions and reasons.

--- conceptdb/test/test_expression.py
+++ conceptdb/test/test_expression.py
@@ -1,23 +1,42 @@
-from conceptdb.assertion import Assertion
-from conceptdb.expression import Expression
+from conceptdb.assertion import Assertion, Expression
 from conceptdb.metadata import Dataset
 import conceptdb
 
 conceptdb.connect_to_mongodb('test')
 
 def test_expression():
+
+    #start clean
+    Expression.drop_collection()
     Assertion.drop_collection() 
     
     a1 = Assertion.make('/data/test','/rel/IsA',
                         ['/concept/test/assertion', '/concept/test/test'])
-    expr = a1.quick_add_expression(
-      Expression.make('{0} is a {1}', ['this assertion', 'test'], 'en')
-    )
+   
+    expr = Expression.make(a1, '{0} is a {1}', ['this assertion', 'test'], 'en')
     
+    #check for consistency, ensure all attributes are readable
+    expr.check_consistency()
+    expr.assertion
+    expr.text
+    expr.confidence
+    expr.arguments
+    expr.language
+    expr.frame
+    expr.id
+
+    assert expr.name == '/expression/%s' % expr.id
+
     # load the same assertion from the DB
     a2 = Assertion.make('/data/test','/rel/IsA',
                         ['/concept/test/assertion', '/concept/test/test'])
-    assert len(a2.expressions) == 1
-    assert a2.expressions[0].text == "this assertion is a test"
+    assert expr.assertion == a2
 
+
+    #test static methods
+    replace = Expression.replace_args(expr.frame, expr.arguments)
+    assert replace == "this assertion is a test"
+
+    #clean up
     Assertion.drop_collection() 
+    Expression.drop_collection()
