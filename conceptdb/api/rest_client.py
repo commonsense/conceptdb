d3e501f8b8f8f85b674e90416566157b4f836d4b
Elizabeth L. Hawkins <elhawk@mit.edu>
2010-10-18 17:17:07 -0400

    find_assertion method still doesn't work.  Not sure why.

--- conceptdb/api/rest_client.py
+++ conceptdb/api/rest_client.py
@@ -13,7 +13,7 @@ except:
     import simplejson as json
 
 
-SERVER_URL = 'http://127.0.0.1:8000' #change when not testing
+SERVER_URL = 'http://127.0.0.1:8000' #NOTE: testing.  
 API_URL = 'http://127.0.0.1:8000/api/'
 CLIENT_VERSION = '1'
 
@@ -21,8 +21,21 @@ CLIENT_VERSION = '1'
 def lookup_assertion(assertion_id):
     return _get_json('assertion', assertion_id)
 
+def lookup_dataset(dataset_name):
+    dataset_name = dataset_name[1:] #strip leading '/'
+    return _get_json(dataset_name)
+
+#FIXME: figure out why this is returning 500
+def find_assertion(dataset, relation, concepts, polarity = '1.0', context = 'None'):
+    dataset = dataset.replace('/', '%2F')
+    concepts = concepts.replace('/', '%2F')
+    relation = relation.replace('/', '%2F')
+    url_append = 'assertionfind?dataset=' + dataset + '&rel=' + relation + '&concepts=' + concepts + '&polarity=' + polarity + '&context=' + context
+    return _get_json(url_append)
+
 def _get_json(*url_parts):
     url = API_URL + '/'.join(urllib2.quote(str(p)) for p in url_parts) + '?format=json'
+    print("url = " + str(url))
     return json.loads(_get_url(url))
 
 
