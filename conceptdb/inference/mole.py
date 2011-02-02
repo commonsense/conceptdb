b5d5e68d05098ff4def262e2e875fd32d5592bf9
Rob Speer <rspeer@mit.edu>
2011-02-02 17:49:21 -0500

    mole algorithm works on a small dataset

---
+++ conceptdb/inference/mole.py
@@ -0,0 +1,122 @@
+# Mole. Two syllables. Like what you put on Mexican food.
+# This is meant to be an extension of SALSA that allows conjunctions, and
+# that doesn't get stuck in local ruts. I'll probably change the name.
+
+# Steps to implement:
+#   - Read in the graph using networkx.
+#   - Iterate over the graph doing the random walk.
+#   - Accumulate results.
+#   - ???
+
+import networkx as nx
+import random
+import codecs
+
+# Constants
+OUT, IN = 1, -1
+
+def load_graph(filename, encoding='utf-8'):
+    return nx.read_edgelist(codecs.open(filename, encoding=encoding),
+                            data=True, delimiter='\t',
+                            create_using=nx.DiGraph())
+
+def run_mole(graph, iterations=100):
+    graph.graph['steps_in'] = 0
+    graph.graph['steps_out'] = 0
+    for node in graph.nodes_iter():
+        graph.node[node]['steps_in'] = 0
+        graph.node[node]['steps_out'] = 0
+        graph.node[node]['hub'] = 1.0 / len(graph)
+        graph.node[node]['authority'] = 0
+    for iter in xrange(iterations):
+        mole_step(graph)
+
+def mole_step(graph):
+    """
+    Do a random walk for every node in the graph, and accumulate hub and
+    authority scores for each node.
+    """
+    
+    for node in graph.nodes_iter():
+        mole_random_walk(graph, node, direction=IN)
+        mole_random_walk(graph, node, direction=OUT)
+
+    total_in = graph.graph['steps_in']
+    total_out = graph.graph['steps_out']
+    for node in graph.nodes_iter():
+        graph.node[node]['hub'] = graph.node[node]['steps_in'] / float(total_in)
+        graph.node[node]['authority'] = graph.node[node]['steps_out'] / float(total_out)
+
+def mole_random_walk(graph, node, direction=None):
+    """
+    Do a random walk from a given node. The direction of the initial step
+    may be specified or chosen randomly.
+
+    If it is not possible to walk in the given direction, the walk will
+    terminate.
+    """
+    if direction is None:
+        direction = random.choice([IN, OUT])
+    
+    if direction == IN:
+        choices = graph.predecessors(node)
+        accumulator = 'steps_in'
+    elif direction == OUT:
+        choices = graph.successors(node)
+        accumulator = 'steps_out'
+    else:
+        raise ValueError("Invalid direction")
+    
+    probability_target = random.random()
+    total_weight = 0.0
+
+    for next_node in choices:
+        total_weight += edge_weight(graph, node, next_node, direction)
+    if total_weight == 0:
+        # Terminate the walk here.
+        return
+
+    probability_used = 0.0
+    for next_node in choices:
+        probability_used += edge_weight(graph, node, next_node, direction) / total_weight
+        if probability_target < probability_used:
+            graph.node[next_node][accumulator] += 1
+            graph.graph[accumulator] += 1
+            return mole_random_walk(graph, next_node)
+    assert False, "Ran out of probability"
+
+def node_weight(graph, node):
+    """
+    What is the total weight on this node, both from its hub score and its
+    authority score?
+    """
+    return graph.node[node]['hub'] + graph.node[node]['authority']
+
+def edge_weight(graph, n1, n2, direction=OUT):
+    """
+    Get the weight of the edge from `n1` to `n2`, taking conjunctions
+    into account.
+    """
+    if direction == IN:
+        n1, n2 = n2, n1
+    edge_data = graph[n1][n2]
+    multiplier = 1.0
+    if 'dependencies' in edge_data:
+        baseline = node_weight(graph, n1)
+        inv_parallel = 0.0
+        for n3 in edge_data['dependencies']:
+            weight = node_weight(graph, n3)
+            if weight == 0:
+                # This depends on a conjunction that cannot be satisfied.
+                return 0.0
+            inv_parallel += 1.0/weight
+        multiplier = 1.0/inv_parallel/baseline
+    return edge_data['weight'] * multiplier
+
+def demo():
+    graph = load_graph('test.graph')
+    run_mole(graph)
+    return graph
+
+if __name__ == '__main__':
+    result = demo()
