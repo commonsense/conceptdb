17924ae09f6fa453587215ac0610f16c72075542
nholm <nholm@mit.edu>
2011-03-29 16:39:08 -0400

    Finished merge capabilities for two different instances of mongodb, and wrote a simple test to make sure it works.

--- conceptdb/db_merge.py
+++ conceptdb/db_merge.py
@@ -3,10 +3,18 @@ from conceptdb.metadata import Dataset
 from conceptdb.assertion import Assertion
 from mongoengine.queryset import QuerySet
 
-# Given two querysets, updates them
-def merge(db1_assertions, db2_assertions):
+'''
+Given two dbs, merges them
+'''
+def merge(db1, db2):
     db1_tobeadded = []
     db2_tobeadded = []
+    
+    conceptdb.connect_to_mongodb(db1)
+    db1_assertions = Assertion.objects
+    
+    conceptdb.connect_to_mongodb(db2)
+    db2_assertions = Assertion.objects
     
     for db1_a in [a1 for a1 in list(db1_assertions) if a1 not in list(db2_assertions)]:
         
@@ -24,7 +32,8 @@ def merge(db1_assertions, db2_assertions):
         for db1_check in list(db1_assertions):
             if assertion_check(db2_a, db1_check):
                 db1_tobeadded.remove(db2_a)
-                
+    
+    conceptdb.connect_to_mongodb(db1)       
     for add1 in db1_tobeadded:
         db1_assertions.create(
                               dataset=db1_assertions[0].dataset,
@@ -34,6 +43,8 @@ def merge(db1_assertions, db2_assertions):
                               context=add1.context,
                               complete=1
                               )
+    
+    conceptdb.connect_to_mongodb(db2)
     for add2 in db2_tobeadded:
         db2_assertions.create(
                               dataset=db2_assertions[0].dataset,
@@ -43,64 +54,72 @@ def merge(db1_assertions, db2_assertions):
                               context=add2.context,
                               complete=1
                               )
+        
     
     return (db1_assertions, db2_assertions)
         
-            
-# Two assertion have the same arguments, relation, polarity, and context                                  
+'''
+Two assertion have the same dataset, arguments, relation, polarity, and context
+'''                                       
 def assertion_check(ass1, ass2):
-    if ass1.argstr==ass2.argstr and ass1.relation==ass2.relation and ass1.polarity==ass2.polarity and ass1.context==ass2.context:
+    if ass1.dataset==ass2.dataset and ass1.argstr==ass2.argstr and ass1.relation==ass2.relation and ass1.polarity==ass2.polarity and ass1.context==ass2.context:
         return True
     return False
-        
-if __name__ == "__main__":
-    
-    conceptdb.connect_to_mongodb('test')
+
+'''
+Creates two test dbs, calls merge
+'''
+def test_dbs(db1, db2):
+    '''
+    Load test assertions into the two DBs:
+    DB1: Assertions 0-9
+    DB2: Assertions 0-4
+    '''
+    conceptdb.create_mongodb(db1)
     Assertion.drop_collection()
-    Dataset.drop_collection()
-    
+    Dataset.drop_collection()  
     for i in xrange(10):
-        Assertion.make('/data/test1','/rel/IsA',['/test/assertion','test/test%d'%i])
-        Assertion.make('/data/test2','/rel/IsA',['/test/assertion','test/test%d'%i])
-        
-    for j in xrange(10):
-        Assertion.make('/data/test1','/rel/HasA',['/test/assertion','test/test%d'%j])
-    
-    
-     # Assertions 0-14, /data/test1 AND /data/test2, AS A QUERYSET
-    db1_assertions = Assertion.objects.filter(dataset='/data/test1')
-    
-    # Assertions 0-9, /data/test1, AS A QUERYSET
-    db2_assertions = Assertion.objects.filter(dataset='/data/test2')
-    
- 
-    print 'DB1 before'
-    for a1 in db1_assertions:
+        Assertion.make('/data/test','/rel/IsA',['/test/assertion','test/test%d'%i])     
+    print "Before the merge, db %s has the following assertions: "%db1
+    for a1 in Assertion.objects:
         print a1
     
-    print 'DB2 before'
-    for a2 in db2_assertions:
-        print a2
-    print 'merging....'
-
-    results = merge(db1_assertions, db2_assertions)
-    db1_assertions = results[0]
-    db2_assertions = results[0]
-    
-    print 'DB1'
-    print type(db1_assertions)
-    for db1 in db1_assertions:
-        print db1
-    
-    print 'DB2'
-    print type(db2_assertions)
-    for db2 in db2_assertions:
-        print db2
-    
-    print 'Assertion.objects'    
-    for a in Assertion.objects:
-        print a
-        
+    conceptdb.create_mongodb(db2)
     Assertion.drop_collection()
     Dataset.drop_collection()
+    for i in xrange(5):
+        Assertion.make('/data/test','/rel/IsA',['/test/assertion','test/test%d'%i])  
+        Assertion.make('/data/test','/rel/HasA',['/test/assertion','test/test%d'%i])     
+    print "Before the merge, db %s has the following assertions: "%db2
+    for a2 in Assertion.objects:
+        print a2
+    
+    '''
+    Merge the two dbs
+    '''
+    merge(db1, db2)
+    
+    '''
+    Check post-merge elements, make sure they match
+    '''
+    conceptdb.connect_to_mongodb(db1)
+    print "After the merge, db %s has the following assertions: "%db1
+    for a4 in Assertion.objects:
+        print a4 
+    Assertion.drop_collection()
+    Dataset.drop_collection()
+    conceptdb.connect_to_mongodb(db2)
+    print "After the merge, db %s has the following assertions: "%db2
+    for a3 in Assertion.objects:
+        print a3
+    Assertion.drop_collection()
+    Dataset.drop_collection()
+        
+    
+    
+if __name__ == "__main__":
+    
+    test_dbs('test1', 'test2')
+    
+    
     
